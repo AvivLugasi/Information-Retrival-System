a76d967c3836aaebb6268269ad62ac0054938b78
Aviv Lugasi <Avivlugasi541@gmail.com>
2022-11-14 20:59:04 +0200

    Added 1 testing for caseFolding, created a testing class for LinguisticProccessor

---
+++ Engine/LinguisticProccessorTester.py
@@ -0,0 +1,111 @@
+import difflib
+
+from LinguisticProccessor import Processor as prc
+from Utils.System import System
+import sys
+
+System = System()
+
+def runCaseFoldingTests(token: str, ling_proccessor: prc):
+    """Call to Processor's caseFolding function"""
+    return ling_proccessor.caseFolding(token)
+
+
+def runRemovePunctuationTests(token: str, ling_proccessor: prc):
+    """Call to Processor's removePunctuation function"""
+    return ling_proccessor.removePunctuation(token)
+
+def runRemoveStopWordsTests(token: str, ling_proccessor: prc):
+    """Call to Processor's removeStopWords function"""
+    return ling_proccessor.removeStopWords(token)
+
+def runPerformStemmingTests(token: str, ling_proccessor: prc):
+    """Call to Processor's performStemming function"""
+    return ling_proccessor.performStemming(token)
+
+def runLinguisticProccessingTests(token: str, ling_proccessor: prc):
+    """Call to Processor's linguisticProccessing function"""
+    return ling_proccessor.linguisticProccessing(token)
+
+
+def performTest(input_file, output_file, ling_proccessor: prc, func):
+    """peroform the required function test for each input and write the result to the output file"""
+    line_num = 1
+    for line in input_file:
+        System.log("perform test for {} line number:{}".format(line, str(line_num)))
+        try:
+            output = func(line, ling_proccessor)
+        except Exception:
+            System.log("Exception was raised")
+            output_file.write(str(Exception) + '\n')
+            return
+        if output != "":
+            line_num += 1
+            output_file.write(output)
+
+def check(output_file, correct_output_file, output_path: str, correct_output_path: str):
+    """Print to the console the differences if exist in the output and the correct output"""
+    System.log("Print differences")
+    output_file_txt = output_file.readlines()
+    correct_output_file_txt = correct_output_file.readlines()
+    for line in difflib.unified_diff(
+            output_file_txt, correct_output_file_txt, fromfile=output_path,
+            tofile=correct_output_path, lineterm=''):
+        System.log(line)
+
+def tester(input_path: str, output_path: str, correct_output_path: str, func_num: int):
+    """perform tests on the required function"""
+    try:
+        input_file = open(input_path, 'r')
+    except FileNotFoundError:
+        System.assertFalse(input_path is None, "input test file is not None")
+        sys.exit(1)
+    try:
+        output_file = open(output_path, 'a+')
+    except FileNotFoundError:
+        System.assertFalse(output_path is None, "output test file is not None")
+        sys.exit(1)
+    try:
+        correct_output_file = open(correct_output_path, 'r')
+    except FileNotFoundError:
+        System.assertFalse(correct_output_path is None, "input test file is not None")
+        sys.exit(1)
+
+    ling_proccessor = prc()
+
+    if func_num == 1:
+        System.log("perform {} tests from{}, writing output to {}".format("CaseFolding", input_path, output_path))
+        func = runCaseFoldingTests
+        performTest(input_file, output_file, ling_proccessor, func)
+    elif func_num == 2:
+        System.log(
+            "perform {} tests from{}, writing output to {}".format("RemovePunctuation", input_path, output_path))
+        func = runRemovePunctuationTests
+        performTest(input_file, output_file, ling_proccessor, func)
+    elif func_num == 3:
+        System.log(
+            "perform {} tests from{}, writing output to {}".format("RemoveStopWords", input_path, output_path))
+        func = runRemoveStopWordsTests
+        performTest(input_file, output_file, ling_proccessor, func)
+    elif func_num == 4:
+        System.log(
+            "perform {} tests from{}, writing output to {}".format("PerformStemming", input_path, output_path))
+        func = runPerformStemmingTests
+        performTest(input_file, output_file, ling_proccessor, func)
+    else:
+        System.log(
+            "perform {} tests from{}, writing output to {}".format("LinguisticProccessing", input_path, output_path))
+        func = runLinguisticProccessingTests
+        performTest(input_file, output_file, ling_proccessor, func)
+
+    System.log("Check differences")
+    output_file.seek(0)
+    check(output_file, correct_output_file, output_path, correct_output_path)
+
+    System.log("Closing Files")
+    input_file.close()
+    output_file.close()
+    correct_output_file.close()
+
+
+tester("Testing/TestsInputs/LinguisticProccessorCaseFolding1.txt", "Testing/TestOutputs/LinguisticProccessorCaseFolding1Out.txt", "Testing/CorrectOutput/LinguisticProccessorCaseFolding1Correct.txt", 1)
