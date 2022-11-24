72cd52aa6d5364d0f2e81507b441232bbf39c9b2
Aviv Lugasi <Avivlugasi541@gmail.com>
2022-11-24 19:05:14 +0200

    Created none positinoal index and positioanl index

--- Engine/Tokenizer.py
+++ Engine/Tokenizer.py
@@ -4,16 +4,6 @@ from Utils.System import System
 
 class Tokenizer(System):
 
-    #subclass for store token-position in current document
-    class TokenPosition:
-
-        def __init__(self, token: str, position: int):
-            """Init a TokenPosition instance"""
-            self.token = token
-            self.position = position # 0 means non-positional index
-        def __str__(self):
-            return "token: " + self.token + " | " + "position: " + str(self.position)
-
     def __init__(self, separators_file: str):
         """Init the Tokenizer class, it reads a separator file and create a list of separators."""
         self.processor = LinguisticProccessor()
@@ -37,6 +27,16 @@ class Tokenizer(System):
         self.log("Close separators file")
         separators.close()
 
+    #subclass for store token-position in current document
+    class TokenPosition:
+
+        def __init__(self, token: str, position: int):
+            """Init a TokenPosition instance"""
+            self.token = token
+            self.position = position # 0 means non-positional index
+        def __str__(self):
+            return "token: " + self.token + " | " + "position: " + str(self.position)
+
     def get_SeparatorsList(self):
         """Return the separators list"""
         return self._separators_list
@@ -44,7 +44,7 @@ class Tokenizer(System):
     def tokenize(self, doc: str, is_positional: bool):
         """Create an inverted index from a given file(positional or not positional)"""
         # open text file
-        self.log("reading text file")
+        self.log("Reading {}".format(doc))
         doc_file = self.readFile(doc, 'r')
 
         # terms dict for positional indexing
@@ -77,7 +77,7 @@ class Tokenizer(System):
                 buffer = buffer + char
 
         # close file
-        self.log("closing text file")
+        self.log("Closing {}".format(doc))
         doc_file.close()
 
         # return the token position pairs list
