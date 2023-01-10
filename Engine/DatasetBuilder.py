d35686206868a75e460e42d15f44af90c78fa513
guyyehezkel <guy.yehezkel01@gmail.com>
2023-01-10 21:43:06 +0200

    update

--- Engine/DatasetBuilder.py
+++ Engine/DatasetBuilder.py
@@ -8,20 +8,45 @@ import pandas as pd
 
 class DatasetBuilder(System):
 
-    def __init__(self, dir_txt_files_path: str, category:int):
+    DIR_FILES_PATH = "../ClassificationModelsDatasets/Category-True"
+
+    def __init__(self, dir_txt_files_path: str, category: int):
 
         list_files_names = self.import_docs(dir_txt_files_path)
+        self.idf_dict = self._idf_dict(dir_files_path=self.DIR_FILES_PATH)
         self.dataset = self.build(dir_txt_files_path, list_files_names, category)
-        
-        
+
     def build(self, dir_txt_files_path: str, list_file_names: str, category:int):
         
         df_list = list()
         for file_name in list_file_names:
-            vector = DocVector(doc_path=dir_txt_files_path + "/" + file_name).vector_representation
+            vector = DocVector(doc_path=dir_txt_files_path + "/" + file_name, idf_dict = self.idf_dict).vector_representation
             vector["Y-LABEL"] = category
             df_list.append(vector)
         return pd.DataFrame.from_records(df_list)
+    
+    def _idf_dict(self, dir_files_path: str):
+        collection_idf_dict = {}
+        files_names = self.import_docs(text_files_path=dir_files_path)
+        # tokenizer instance
+        tokenizer = Tokenizer(separators_file="Utils/separetors.txt")
+        # linguistic Processor instance
+        lg = LinguisticProcessor()
+        for file in files_names:
+            # doc = self.read_file(file_path=dir_files_path+ "/" + file, mode='r')
+            tokens_dict = tokenizer.tokenize(doc=dir_files_path+ "/" + file)
+            doc_terms_dict = lg.preform_processing(tokens_dict=tokens_dict)
+            for term in doc_terms_dict.keys():
+                if term in collection_idf_dict:
+                    collection_idf_dict[term] +=1
+                else:
+                    collection_idf_dict[term]=1
+        # In this point we have (term, df) pairs for all terms in the collection
+        for term, df in collection_idf_dict.items():
+            print(files_names)
+            collection_idf_dict[term]= math.log(df/len(files_names))
+        # In this point we have (term, idf) pairs for all terms in the collection
+        return collection_idf_dict
 # main
 
 # create False category dataset
--- Engine/DocVector.py
+++ Engine/DocVector.py
@@ -3,17 +3,20 @@ from Tokenizer import Tokenizer
 from LinguisticProcessor import LinguisticProcessor
 from joblib import Parallel, delayed
 import joblib
+import math
 
 SEPARATORS_FILES = "Utils/separetors.txt"
 LANGUAGE_MODEL_PATH = 'LanguageModels/languageModelByTerms.pkl'
 
 class DocVector(System):
 
-    def __init__(self, doc_path: str):
+    def __init__(self, doc_path: str, idf_dict: dict):
         # tokenizer instance
         tokenizer = Tokenizer(separators_file=SEPARATORS_FILES)
         # linguistic Processor instance
         lg = LinguisticProcessor()
+
+        self.idf_dict = idf_dict
 
         tokens_dict = tokenizer.tokenize(doc=doc_path)
         terms_dict = lg.preform_processing(tokens_dict=tokens_dict)
@@ -27,28 +30,21 @@ class DocVector(System):
 
     def _build_doc_vector(self, terms_dict: dict):
         """
-        This function get terms dictionary and return vector who represent the dictionary.
-         A value of a term is the probability that the term was created from the category collection language model,
-          times the term frequency in the input doc.
+        This function get terms dictionary and return vector which represent the document.
+         A value of a term is normalized tf (in doc input) times idf,
         :param terms_dict: {term, doc_term_frequency}
         :return: vector representation as python dictionary
         """
-        collection_language_model = joblib.load(LANGUAGE_MODEL_PATH)
-        vector = collection_language_model.copy()
-        for term, collection_prob in collection_language_model.items():
-            doc_tf = terms_dict[term] if term in terms_dict else 0
-            vector[term] = collection_prob * doc_tf
+        # collection_language_model = joblib.load(LANGUAGE_MODEL_PATH)
+        # vector = collection_language_model.copy()
+        vector = self.idf_dict.copy()
+        for term, idf in self.idf_dict.items():
+            tf = math.log(terms_dict[term]) if term in terms_dict else 0
+            vector[term] = idf * tf
         return vector
-    
-    # def _generate_datset(self, vector: dict):
-    # 
-    # # def _add_label_sample(self, vector: dict, label: bool):
-    # #     vector[Y-label] = label
-    #
-
-
 
 # Main
+
 # doc_path="../ArtificialIntelligenceExplainability/text/A_Survey_of_Contrastive_and_Counterfactual_Explanation_Generation_Methods_for_Explainable_Artificial_Intelligence.txt"
 # vector_doc = DocVector(doc_path=doc_path)
 # print(vector_doc.vector_representation)
